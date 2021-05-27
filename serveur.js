9ef72478391f54150ab5c606af63d82a8d9c457d
XeuyNTLO <cbabacardiouf@gmail.com>
2021-05-27 15:24:33 +0000

    commit 6

--- serveur.js
+++ serveur.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const path = require('path') ;
 const app = express() ;
-app.use(express.static(__dirname + '/dist/cnepsfront')) ;
+app.use(express.static(__dirname + '/dist/cnepsFront')) ;
 app.get('/*', function(req,res){
-    res.sendfile(path.join(__dirname+'/dist/cnepsfront/index.html'));
+    res.sendfile(path.join(__dirname+'/dist/cnepsFront/index.html'));
 }) ;
 
 app.listen(process.env.PORT) ;
